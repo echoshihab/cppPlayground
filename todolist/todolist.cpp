9557f71077032b078c003d3d62d09cb7e66264f0
Shihab <echoshihab@gmail.com>
2022-04-06 20:01:23 -0400

    persist todos to sqlite db on exit

--- todolist/todolist.cpp
+++ todolist/todolist.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include "sqlite3.h"
 
 using namespace std;
 
@@ -25,11 +26,43 @@ int main()
 		}
 	}
 
-	// print out item and write to external file.
+	// print out item and write to external file
+	// also write to db.
 	cout << "Todo list for today:" << endl;
 	
 	
 	ofstream todoFile;
+	sqlite3 *sdb;
+	sqlite3_stmt *stmtTbl;
+	sqlite3_stmt *stmtInsert;
+
+	string createTableStatement = "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, todo TEXT);";
+	
+
+	//initialize 
+	if(SQLITE_OK != (sqlite3_initialize()))
+	{
+	  cout	<< "Failed to initialize sqlite3 \n";	
+	}
+
+	//open connection to db
+	if (SQLITE_OK !=( sqlite3_open("cppTest.db", &sdb))) 
+	{
+ 	  cout << "Failed to open connection \n";
+	}
+
+	//create table if not exists
+	if(SQLITE_OK != (sqlite3_prepare(sdb, createTableStatement.c_str(), createTableStatement.size(), &stmtTbl, NULL)))
+	{
+		cout << "Failed to prepare create table statement " << sqlite3_errmsg(sdb);
+	}
+
+	
+	if(SQLITE_DONE != (sqlite3_step(stmtTbl)))
+	{
+		cout << "Failed to create table" << sqlite3_errmsg(sdb);
+	}
+
 
 	todoFile.open("todo.txt");	
 
@@ -41,8 +74,34 @@ int main()
 			todoFile << item ;
 			todoFile << "\n";	
 		}	
+		
+		string insertStatement = "INSERT INTO todos (todo) VALUES ('"+item+"');";
+		//INSERT TO DB AS WELL
+		if(SQLITE_OK != (sqlite3_prepare(sdb, insertStatement.c_str(), insertStatement.size(), &stmtInsert, NULL)))
+		{
+			cout << "Failed to prepare insert " << sqlite3_errmsg(sdb);  
+		}
+
+		if(SQLITE_DONE != (sqlite3_step(stmtInsert)))
+		{
+			cout << "Failed to insert " << sqlite3_errmsg(sdb) << '\n'; 
+		}
+
+
 	}
+
+	//cleanup
 	todoFile.close();
+	if(stmtTbl != NULL) {
+		sqlite3_finalize(stmtTbl);
+	}
+	if(stmtInsert != NULL){
+		sqlite3_finalize(stmtInsert);
+	}
+	if(sdb != NULL) {
+		sqlite3_close(sdb);
+	}
+	sqlite3_shutdown();
 
 	return 0;
 
