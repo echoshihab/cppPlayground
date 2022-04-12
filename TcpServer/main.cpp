ad3485a971f7875c8e379d0c4cbd9263a7a80c0a
Shihab Khan <echoshihab@gmail.com>
2022-04-11 20:31:27 -0400

    create socket

---
+++ TcpServer/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+void main()
+{
+	// Initialize winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOK = WSAStartup(ver, &wsData);
+
+	if (wsOK != 0)
+	{
+		std::cerr << "Can't Initialize winsock. Quitting \n";
+		return;
+	}
+
+	// Create a socket
+	SOCKET listeningSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (listeningSocket == INVALID_SOCKET)
+	{
+		std::cerr << "Can't create a socket! Quitting \n";
+		return;
+	}
+	
+	// Bind the socket to an ip address and port
+	
+
+	//Tell winsock the socket is for listening
+	
+	// Wait for a connection
+
+	// Close listening socket
+
+	// loop through accept and echo message back to client
+	
+	// Close the socket
+	
+	// Shutdown winsock
+
+
+
+}
