4c3cf6169c458cf851152e5d4b688ac6764febd0
Shihab Khan <echoshihab@gmail.com>
2022-04-13 09:00:14 -0400

    establish listening socket and wait for connection

--- TcpServer/main.cpp
+++ TcpServer/main.cpp
@@ -3,6 +3,8 @@
 
 #pragma comment (lib, "ws2_32.lib")
 
+//reference - https://www.youtube.com/watch?v=WDn-htpBlnU
+
 void main()
 {
 	// Initialize winsock
@@ -24,18 +26,46 @@ void main()
 		std::cerr << "Can't create a socket! Quitting \n";
 		return;
 	}
-	
-	// Bind the socket to an ip address and port
+
+	// Bind the ip address and port to a socket
 	sockaddr_in hint;
 	hint.sin_family = AF_INET;
 	hint.sin_port = htons(5400);
 	hint.sin_addr.S_un.S_addr = INADDR_ANY;
-	
+
 	bind(listeningSocket, (sockaddr*)&hint, sizeof(hint));
 
 	//Tell winsock the socket is for listening
-	
+	listen(listeningSocket, SOMAXCONN);
+
+
 	// Wait for a connection
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listeningSocket, (sockaddr*)&client, &clientSize);
+	if (clientSocket == INVALID_SOCKET) {
+		std::cerr << "Can't accept a socket connection, Quitting \n";
+		return;
+	}
+
+	char host[NI_MAXHOST]; //client's remote name
+	char service[NI_MAXHOST]; // service (i.e. port_ the client is connecting on
+
+	ZeroMemory(host, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXHOST);
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		std::cout << host << " connected on port " << service << '\n';	
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		std::cout << host << " connected on port " <<
+			ntohs(client.sin_port) << '\n';
+	}
+	
 
 	// Close listening socket
 
