83506ab86e0ba7dea14ecdbe1a9e4526d6fdf5e2
Shihab Khan <echoshihab@gmail.com>
2022-07-05 17:02:49 -0400

    implement peek and complete stack implementation using dynamically growing array

--- data_structures/stack/Stack/Stack.cpp
+++ data_structures/stack/Stack/Stack.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 
 class DynamicArrayStack
 {
@@ -7,49 +8,67 @@ class DynamicArrayStack
 private:
 	int sizeOfArray{ 4 };
 	int totalElements{ 0 };
-	int* dynamicArr = new int[4]{};
+	int* dynamicArr = new int[4];
 
 public:
 	void increaseArrayCapacity() {
-		int* tempArray = new int[sizeOfArray*2] {};
+		//double the size of array when no more space left
+		int* tempArray = new int[sizeOfArray*2];
 		//copy all elements of current array to new temp array
 		std::copy(dynamicArr, dynamicArr+sizeOfArray, tempArray);
-		// delete existing array
+
 		delete[] dynamicArr;
 		// set existing array to the temp array
 		dynamicArr = tempArray;
-		//delete the temp array
+		
 		delete[] tempArray;
+
 	}
 	void push(int val) {
-		if (totalElements < sizeOfArray)
+		std::cout << "Pushing " << std::to_string(val) << " into stack" << '\n';
+		if (totalElements <= sizeOfArray + 1)
 		{
-			dynamicArr[totalElements + 1] = val;
+			dynamicArr[totalElements] = val;
 			totalElements += 1;
 		}
 		else
 		{
-			std::cout << "Can't push any more elements to stack" << '\n';
+			increaseArrayCapacity();
+			dynamicArr[totalElements] = val;
+			totalElements += 1;
 		}
+		peek();
 	}
 
+
 	int pop() {
+		//this doesn't remove the element from array completely, but it creates the illusion by decreasing number of 
+		//total elements
 		if (totalElements > 0)
 		{
-			int currentNumOfElements = totalElements;
+			int indexOfItemToBeRemoved = totalElements-1;
 			totalElements -= 1;
-			return dynamicArr[currentNumOfElements];
+			std::cout << "Item being Popped: " << dynamicArr[indexOfItemToBeRemoved] << '\n';
+			peek();
+			return dynamicArr[indexOfItemToBeRemoved];
+
 
 		}
 		else
 		{
-			std::cout << "Can't pop any more elements from stack";
+			std::cout << "Can't pop any more elements from stack" << '\n';
 		}
+
+	}
+
+	void peek() {
+		int indexOfTopItem = totalElements - 1;
+		std::cout << "top item is " << std::to_string(dynamicArr[indexOfTopItem])  << '\n';
 	}
 
 	void printStack() {
-		std::cout << "Current Stack: ";
-		for (int i = 0; i < totalElements; i++) {
+		std::cout << "Current Stack: " << '\n';
+		for (int i = 0; i <= totalElements; i++) {
 			std::cout << dynamicArr[i] << '\n';
 		}
 	}
@@ -59,18 +78,18 @@ public:
 
 int main()
 {
-	std::cout << "Hello";
 
 	DynamicArrayStack d;
 
 	d.push(5);
 	d.push(4);
+	d.push(3);
+	d.push(2);
 	d.push(1);
-
-	std::cout << "Item Popped: " << d.pop() << '\n';
-	std::cout << "Item Popped: " << d.pop() << '\n';
-	std::cout << "Item Popped: " << d.pop() << '\n';
+	d.push(0);
+	d.pop();
 	d.printStack();
+
 	return 0;
 }
  
